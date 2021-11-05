1840e328c7ab3b2dfcb8ead1d71ad7bd22fbfe92
Tristan David Goodrich <tgoodric@users.noreply.github.com>
2021-11-05 15:45:11 -0400

    Further implementation
    Now if only I didn't suck at CSS/Bootstrap I could get it to look pretty

--- marketplace-FE/src/app/components/detail/detail.component.ts
+++ marketplace-FE/src/app/components/detail/detail.component.ts
@@ -10,7 +10,13 @@ export class DetailComponent implements OnInit {
   constructor() { }
 
   descriptionText:string = "placeholder to be changed by some sort of api service or something idk";
-  
+  cardTitle:string = "placeholder title";
+  benefit1:string = "placeholder for benefit 1";
+  benefit2:string = "placeholder for benefit 2";
+  benefit3:string = "placeholder for benefit 3";
+  benefit1Description:string = "placeholder for benefit 1 description";
+  benefit2Description:string = "placeholder for benefit 1 description";
+  benefit3Description:string = "placeholder for benefit 1 description";
 
   ngOnInit(): void {
   }
